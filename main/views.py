cfd2bf82ffe9efdff14acaeb282b8995637f9665
Najwa Salsabil <najwa.salsabil@ui.ac.id>
2026-09-08 09:21:01 +0700

    Tutorial 2

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Najwa Salsabil",
+        "npm": "2506588701",
+        "study_program": "S1 Information System",
+        "bio": (
+            "Information Systems student at Universitas Indonesia passionate about web development, technology exploration, and Business. Currently learning to build clean, functional, and scalable applications through modern web platforms. Always excited to tackle new challenges and collaborate on impactful projects."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Najwa Salsabil",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)

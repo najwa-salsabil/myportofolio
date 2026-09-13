aa4b17a50484f0385b96f014262183439f548b14
Najwa Salsabil <najwa.salsabil@ui.ac.id>
2026-09-13 09:40:10 +0700

    Add Projects and Achievements templates

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from main.views import show_main, show_experience
+from main.views import show_main, show_experience, show_projects, show_achievements
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("projects/", show_projects, name="show_projects"),
+    path("achievements/", show_achievements, name="show_achievements"),
 ]

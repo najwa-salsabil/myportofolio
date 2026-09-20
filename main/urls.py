8606c0c13e13005764bed42da7281ab1ee7c9333
Najwa Salsabil <najwa.salsabil@ui.ac.id>
2026-09-20 11:36:46 +0700

    Add create, edit, and delete actions to Experience page

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,10 @@ from main.views import (
     create_project,
     get_projects_json,
     delete_project,
+    create_experience,
+    update_experience,
+    delete_experience,
+    get_experience_json,
 )
 
 app_name = "main"
@@ -20,4 +24,8 @@ urlpatterns = [
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/edit/", update_experience, name="update_experience"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
 ]
